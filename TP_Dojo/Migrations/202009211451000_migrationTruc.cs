17f1e197697c4c2c1a8904bbde24e5ead599b2ac
Amandine FOULET <amandine.foulet2020@campus-eni.fr>
2020-09-21 16:59:31 +0200

    create SamouraiVM

---
+++ TP_Dojo/Migrations/202009211451000_migrationTruc.cs
@@ -0,0 +1,16 @@
+namespace TP_Dojo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class migrationTruc : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ TP_Dojo/Models/SamouraiVM.cs
@@ -0,0 +1,17 @@
+﻿using BO_Dojo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP_Dojo.Models
+{
+    public class SamouraiVM
+    {
+        public Samourai Samourai { get; set; }
+
+        public List<Arme> ListeArmes { get; set; }
+
+        public int? ArmeId { get; set; }
+    }
+}
